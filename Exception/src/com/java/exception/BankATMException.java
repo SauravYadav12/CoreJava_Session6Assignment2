ef4d89952e0435ce0dee96ddc4a69d4a0e893422
SauravYadav12 <saurav1286@gmail.com>
2017-07-02 09:03:36 +0530

    Update BankATMException.java

--- Exception/src/com/java/exception/BankATMException.java
+++ Exception/src/com/java/exception/BankATMException.java
@@ -1,10 +1,11 @@
 package com.java.exception;
 
+@SuppressWarnings("serial")
 public class BankATMException extends Exception  {
 
 
 	public BankATMException(String string) {
-		
+		super(string);
 	}
 		
 }
